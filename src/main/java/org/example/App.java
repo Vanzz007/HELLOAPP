1198ffbc0db937966f8f3f40eba251121158a792
Vanz-007 <vv2902@srmist.edu.in>
2026-04-05 20:57:16 +0530

    Added UC2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,17 @@
 package org.example;
+public class App{
 
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    public static void main(String[] args) {
+
+        // UC1: Hello World (default)
+        if (args.length == 0) {
+            System.out.println("Hello, User!");
+        }
+        // UC2: Personalized Greeting
+        else {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        }
     }
 }
